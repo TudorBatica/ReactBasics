77589b493e0c55e5e48e70d72acb72a598a9a8d3
Tudor <tudor.batica@gmail.com>
2021-02-05 21:45:54 +0200

    redirects and delete requests

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,18 +1,42 @@
-import useFetch from "../hooks/useFetch"
+import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setDescription] = useState('');
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //prevent page refresh
+        const task = { title, body } 
+        console.log(task);
+        
+        fetch('http://localhost:8000/tasks', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(task)
+        }).then(() => {
+            console.log("new task added");
+            history.go(-1);
+        });
+    }
+
     return (
         <div className="create">
             <h2> Add new task </h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Title:</label>
                 <input 
                     type="text"
                     required
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
                 />
                 <label>Description:</label>
                 <textarea
                     required
+                    value={body}
+                    onChange={(event) => setDescription(event.target.value)}
                 />
                 <button>Add Task</button>
             </form>
--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -1,9 +1,18 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 
 const TaskDetails = () => {
     const { id } = useParams();
     const { data: task, error, isLoading } = useFetch(`http://localhost:8000/tasks/${id}`);
+    const history = useHistory();
+    
+    const handleClick = () => {
+        fetch(`http://localhost:8000/tasks/${id}`, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        });
+    }
 
     return (  
         <div className="task-details">
@@ -13,6 +22,7 @@ const TaskDetails = () => {
                 <article>
                     <h2>{task.title}</h2>
                     <p>{task.body}</p>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
