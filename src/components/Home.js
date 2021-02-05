11863950a529ea1cb0fd06146ec9b9cf60145b8d
Tudor <tudor.batica@gmail.com>
2021-02-05 15:24:29 +0200

    events

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+    
+    const handleClick = (name, e) => {
+        console.log(`clique, ${name}; ${e.target}`);
+    }
+
     return (
         <div className="home">
             <h2>List</h2>
+            <button onClick = {(e) => handleClick('playuh', e)}>Click Me!</button>
         </div>
     );
 }
